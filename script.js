3fe7bad7b75e6dae722b735890eef2c6717f1df2
Heitor Calaça <heitor.calaca@gmail.com>
2023-11-07 16:42:25 -0300

    .

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fillTable("./data.json", "tBody");
+  fillTable("http://localhost:3000/data.json", "tBody");
 });
 
 document
   .getElementById("formNova")
   .addEventListener("submit", handleFormSubmit);
+
+document.addEventListener(
+  "click",
+  async function (event) {
+    if (event.target.closest(".btn-excluir")) {
+      const id = event.target.closest(".btn-excluir").getAttribute("data-id");
+      await deleteData(id);
+    }
+  },
+  false
+);
+
+// document
+//   .getElementById("formEdicao")
+//   .addEventListener("submit", handleEditSubmmit);
+
+document.getElementById("numero").addEventListener("input", function (e) {
+  if (e.target.value.length < 3) {
+    e.target.setCustomValidity("Por favor, insira pelo menos 3 dígitos.");
+  } else {
+    e.target.setCustomValidity("");
+  }
+});
+
+//função para preencher formulário de edição
+async function fillEditForm(id) {
+  const response = await fetch(`http://localhost:3000/data.json/${id}`);
+  const data = await response.json();
+}
 
 //função para preencher a tabela
 async function fillTable(url, tableId) {
@@ -28,7 +57,7 @@ async function fillTable(url, tableId) {
             </button>
           </a>
           <a>
-            <button class="btn btn-excluir">
+            <button class="btn btn-excluir" data-id="${obj.id}">
               <span class="material-icons">delete</span>
             </button>
           </a>`;
@@ -43,12 +72,23 @@ async function handleFormSubmit(event) {
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
+  // Adicione um ID único ao objeto de dados
+  data.id = "id_" + Math.random().toString(36).substr(2, 9);
+
+  const selectElement = document.getElementById("situacao");
+  data.situacao = selectElement.options[selectElement.selectedIndex].text;
+  const selectElement2 = document.getElementById("caracteristicas");
+  data.caracteristicas =
+    selectElement2.options[selectElement2.selectedIndex].text;
+  const selectElement3 = document.getElementById("situacaoMae");
+  data.situacaoMae = selectElement3.options[selectElement3.selectedIndex].text;
+
   const response = await sendDataToServer(data);
 
   if (response.ok) {
     console.log("Registro adicionado com sucesso!");
     alert("Registro adicionado com sucesso!");
-    window.location.reload();
+    window.location.href = "#";
   } else {
     alert("Erro ao adicionar registro!");
   }
@@ -58,6 +98,56 @@ async function handleFormSubmit(event) {
 async function sendDataToServer(data) {
   return await fetch("http://localhost:3000/data.json", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+//função para excluir um registro
+async function deleteData(id) {
+  const response = await fetch(`http://localhost:3000/data.json/${id}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    alert("Registro excluído com sucesso!");
+    window.location.href = "#";
+  } else {
+    alert("Erro ao excluir registro!");
+  }
+}
+
+//função para editar um registro
+async function handleEditSubmmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  const selectElement = document.getElementById("situacao");
+  data.situacao = selectElement.options[selectElement.selectedIndex].text;
+  const selectElement2 = document.getElementById("caracteristicas");
+  data.caracteristicas =
+    selectElement2.options[selectElement2.selectedIndex].text;
+  const selectElement3 = document.getElementById("situacaoMae");
+  data.situacaoMae = selectElement3.options[selectElement3.selectedIndex].text;
+
+  const response = await editData(data.id, data);
+
+  if (response.ok) {
+    console.log("Registro editado com sucesso!");
+    alert("Registro editado com sucesso!");
+    window.location.href = "#";
+  } else {
+    alert("Erro ao editar registro!");
+  }
+}
+
+async function editData(id, data) {
+  const response = await fetch(`http://localhost:3000/data.json/${id}`, {
+    method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.get("/data.json", (req, res) => {
   });
 });
 
+app.get("/data.json/:id", (req, res) => {
+  const id = req.params.id;
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Erro ao ler o arquivo");
+    } else {
+      res.send(JSON.parse(data).find((item) => item.id === id));
+    }
+  });
+});
+
 app.post("/data.json", (req, res) => {
   // Verifique se os dados enviados são um objeto e não um array
   if (typeof req.body !== "object" || Array.isArray(req.body)) {
@@ -38,7 +49,6 @@ app.post("/data.json", (req, res) => {
 
     // Adicione os novos dados ao array existente
     jsonData.push(req.body);
-    console.log(req.body);
 
     const newData = JSON.stringify(jsonData, null, 2);
 
@@ -49,6 +59,76 @@ app.post("/data.json", (req, res) => {
       }
 
       res.send("Arquivo atualizado com sucesso");
+    });
+  });
+});
+
+//falta terminar
+app.put("/data.json/:id", (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile("./data.json", "utf8", (readErr, data) => {
+    if (readErr) {
+      console.log("Error reading file:", readErr);
+      return res.status(500).send("Erro ao ler o arquivo");
+    }
+
+    let jsonData;
+
+    try {
+      jsonData = JSON.parse(data);
+    } catch (jsonErr) {
+      return res.status(500).send("Erro ao analisar o arquivo JSON");
+    }
+    // Encontre o índice do item com o id fornecido
+    const index = jsonData.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      return res.status(400).send("Item não encontrado");
+    }
+
+    // Atualize o item no array
+    console.log(jsonData[index]);
+    jsonData[index] = req.body;
+    console.log(jsonData[index]);
+  });
+});
+
+app.delete("/data.json/:id", (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile("./data.json", "utf8", (readErr, data) => {
+    if (readErr) {
+      console.log("Error reading file:", readErr);
+      return res.status(500).send("Erro ao ler o arquivo");
+    }
+
+    let jsonData;
+    try {
+      jsonData = JSON.parse(data);
+    } catch (parseErr) {
+      return res.status(500).send("Erro ao analisar o arquivo JSON");
+    }
+
+    // Encontre o índice do item com o id fornecido
+    const index = jsonData.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      return res.status(400).send("Item não encontrado");
+    }
+
+    // Remova o item do array
+    jsonData.splice(index, 1);
+
+    const newData = JSON.stringify(jsonData, null, 2);
+
+    fs.writeFile("./data.json", newData, "utf8", (writeErr) => {
+      if (writeErr) {
+        console.log("Error writing file:", writeErr);
+        return res.status(500).send("Erro ao atualizar o arquivo");
+      }
+
+      res.send("Item excluído com sucesso");
     });
   });
 });
